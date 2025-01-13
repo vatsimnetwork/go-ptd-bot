bf46166b2b9245744a9998566b93e3738a155861
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2025-01-13 10:55:30 -0800

    add sentry for error reporting

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
+	"log"
 	"ptd-discord-bot/internal/bot"
+	"ptd-discord-bot/internal/config"
+	"time"
 )
 
 func main() {
 	bot.Run()
+	if config.Env == "production" {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:         config.SentryDSN,
+			Debug:       false,
+			Environment: config.Env,
+		})
+		if err != nil {
+			log.Fatalf("sentry.Init: %s", err)
+		}
+		defer sentry.Flush(2 * time.Second)
+	}
 }
